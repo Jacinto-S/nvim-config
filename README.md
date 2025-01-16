1eec23a9d0ad8b49653f867b14bd003f1c2692e9
jschw001 <jacinto.schwarzwaelder@student.hs-rm.de>
2025-01-16 21:46:49 +0100

    Fixed airline symbols

--- README.md
+++ README.md
@@ -1,12 +1,18 @@
 # Neovim Config
 
 ### ToDo:
-- Git diff like in IDEs
-- Debugger support
-- Better shortcuts for undotree
+
 - Better support for code completion and tweaks to code suggestions (no spelling...)
+- Git diff like in IDEs
 - Airline fix
-- Actual coding to find more wishes
+
+- Better shortcuts for undotree
 - Code execution shortcut
-- crtl + w shortcuts (resize, easier navigation...)
+- crtl + w shortcuts (resize, easier navigation...) crtl + w >
+- Shortcut zum einrücken besser
+
 - support for testing
+- Debugger support
+
+- Actual coding to find more wishes
+- cp from vimtree
--- after/plugin/airline.lua
+++ after/plugin/airline.lua
@@ -3,45 +3,51 @@ if not vim.g.airline_symbols then
     vim.g.airline_symbols = {}
 end
 
+-- :h lua-vim-variables -> copies not references, for some weird reason
+local airline_symbols = vim.g.airline_symbols
+
 -- unicode symbols
 vim.g.airline_left_sep = '»'
 vim.g.airline_left_sep = '▶'
 vim.g.airline_right_sep = '«'
 vim.g.airline_right_sep = '◀'
-vim.g.airline_symbols.colnr = ' ㏇:'
-vim.g.airline_symbols.colnr = ' ℅:'
-vim.g.airline_symbols.crypt = '🔒'
-vim.g.airline_symbols.linenr = '☰'
-vim.g.airline_symbols.linenr = ' ␊:'
-vim.g.airline_symbols.linenr = ' ␤:'
-vim.g.airline_symbols.linenr = '¶'
-vim.g.airline_symbols.maxlinenr = ''
-vim.g.airline_symbols.maxlinenr = '㏑'
-vim.g.airline_symbols.branch = '⎇'
-vim.g.airline_symbols.paste = 'ρ'
-vim.g.airline_symbols.paste = 'Þ'
-vim.g.airline_symbols.paste = '∥'
-vim.g.airline_symbols.spell = 'Ꞩ'
-vim.g.airline_symbols.notexists = 'Ɇ'
-vim.g.airline_symbols.notexists = '∄'
-vim.g.airline_symbols.whitespace = 'Ξ'
+airline_symbols.colnr = ' ㏇:'
+airline_symbols.colnr = ' ℅:'
+airline_symbols.crypt = '🔒'
+airline_symbols.linenr = '☰'
+airline_symbols.linenr = ' ␊:'
+airline_symbols.linenr = ' ␤:'
+airline_symbols.linenr = '¶'
+airline_symbols.maxlinenr = ''
+airline_symbols.maxlinenr = '㏑'
+airline_symbols.branch = '⎇'
+airline_symbols.paste = 'ρ'
+airline_symbols.paste = 'Þ'
+airline_symbols.paste = '∥'
+airline_symbols.spell = 'Ꞩ'
+airline_symbols.notexists = 'Ɇ'
+airline_symbols.notexists = '∄'
+airline_symbols.whitespace = 'Ξ'
 
 -- powerline symbols
 vim.g.airline_left_sep = ''
 vim.g.airline_left_alt_sep = ''
 vim.g.airline_right_sep = ''
 vim.g.airline_right_alt_sep = ''
-vim.g.airline_symbols.branch = ''
-vim.g.airline_symbols.colnr = ' ℅:'
-vim.g.airline_symbols.readonly = ''
-vim.g.airline_symbols.linenr = ' :'
-vim.g.airline_symbols.maxlinenr = '☰ '
-vim.g.airline_symbols.dirty='⚡'
+airline_symbols.branch = '' --  
+airline_symbols.colnr = 'C:'
+airline_symbols.readonly = ''
+airline_symbols.linenr = ' L:'
+airline_symbols.maxlinenr = ' ☰ '
+airline_symbols.dirty=' ' -- ⚡
+
+vim.g.airline_symbols = airline_symbols
 
 vim.g.airline_theme = 'catppuccin'
 vim.g['airline#extensions#tabline#enabled'] = 1
 vim.g['airline#extensions#tabline#formatter'] = 'unique_tail'
 vim.g.airline_powerline_fonts = 1
+
 
 vim.cmd('AirlineRefresh')
 
