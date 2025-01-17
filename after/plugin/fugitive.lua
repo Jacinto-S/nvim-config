4894767c649db9c56f481e2914f87fa49f460ba3
jschw001 <jacinto.schwarzwaelder@student.hs-rm.de>
2025-01-17 11:46:38 +0100

    Adding git pull shortcut

--- after/plugin/fugitive.lua
+++ after/plugin/fugitive.lua
@@ -4,6 +4,7 @@ vim.keymap.set('n', '<leader>gr', '_wy$:Git restore --staged <C-r>0<Enter>', { n
 vim.keymap.set('n', '<leader>gx', '_wy$:Git rm --cached <C-r>0<Enter>', { noremap = true, desc = "Git remove file" })
 vim.keymap.set('n', '<leader>gc', ':Git commit -m ""<Left>', { noremap = true, desc = "Git commit" })
 vim.keymap.set('n', '<leader>gp', ':Git push<Enter>', { noremap = true, desc = "Git push" })
+vim.keymap.set('n', '<leader>gg', ':Git pull<Enter>', { noremap = true, desc = "Git pull (get)" })
 vim.keymap.set('n', '<leader>gf', ':Git fetch<Enter>', { noremap = true, desc = "Git fetch" })
 vim.keymap.set('n', '<leader>gi', '_wy$:aboveleft split <C-r>0<Enter>', { noremap = true, desc = "Inspect file" })
 
