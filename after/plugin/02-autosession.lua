e51ce688ff5f83aba0646fbbda923ea0e3d9118c
Jacinto Schwarzwälder <schwarzwaelder.jacinto@gmail.com>
2025-01-29 19:52:34 +0100

    Autosession only activate if exists

--- after/plugin/02-autosession.lua
+++ after/plugin/02-autosession.lua
@@ -1,5 +1,8 @@
-vim.cmd("SessionRestore")
-vim.cmd("NvimTreeOpen")
+local autosession = require("auto-session")
+
+if autosession.session_exists_for_cwd() then
+    vim.cmd("SessionRestore")
+    vim.cmd("NvimTreeOpen")
+end
 
 vim.o.sessionoptions="blank,buffers,curdir,folds,help,tabpages,winsize,winpos,terminal,localoptions"
-
