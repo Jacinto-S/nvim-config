ceffff28652130e982d59c755c433d32f54a139f
Jacinto Schwarzwälder <schwarzwaelder.jacinto@gmail.com>
2025-01-29 20:30:08 +0100

    Adding icons to leader commands with which-key

--- after/plugin/whichkey.lua
+++ after/plugin/whichkey.lua
@@ -1,9 +1,21 @@
 local wk = require("which-key")
+
 wk.add({
     { "<leader>d", group = "Debugger"},
     { "<leader>g", group = "Git"},
     { "<leader>p", group = "Project Search"},
-    { "<leader>r", group = "Remove", icon=""},
+    { "<leader>r", group = "Remove", icon = { icon = "⌫", color = "red"} },
     { "<leader>t", group = "Toggle"},
-    { "<leader>_", group = "Comment", icon="//"},
+    { "<leader>_", group = "Comment", icon = { icon="//", color = "orange"}},
+    { "<leader>v", icon = { icon = "", color="yellow" } },
+    { "<leader>h", icon = { icon = "", color = "grey" } },
+    { "<leader>c", icon = { icon = "", color = "orange"} },
+    { "<leader>a", icon = { icon = "", color = "green"} },
+    { "<leader>n", icon = { icon = "󱏒", color = "azure"} },
+    { "<leader>s", icon = { icon = "󰛔", color = "azure"} },
+    { "<leader>x", icon = { icon = "⌫", color = "red"} },
+    { "<leader>rm", icon = { icon = "⌫", color = "red"} },
 })
+
+-- Color Reference: azure, blue, cyan, green, grey, orange, purple, red, yellow
+
